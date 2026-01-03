d6d70f6704c7b9458629de52c8195fea4f56326b
muresaijaideepreddy <saijaideepreddymure@tamu.edu>
2026-01-03 16:09:17 -0600

    created an planner class

---
+++ planner.py
@@ -0,0 +1,17 @@
+def planner(repo_event: dict)-> dict:
+    """
+    These take the input as the 
+    repo_event :
+    changed files 
+    issue_severity
+    issue_description
+
+
+    output :
+    modules_test
+    test_types
+    risk_level 
+
+
+    """
+    pass
